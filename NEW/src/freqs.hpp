afa14e3991a36c4f8a923c7d7b59bfeeb839fff7
xanthos@home <xanthos@mail.ntua.gr>
2015-03-31 23:17:04 +0300

    experiments on observation type templates

--- NEW/src/freqs.hpp
+++ NEW/src/freqs.hpp
@@ -8,25 +8,25 @@ namespace ngpt {
 /** @brief Generalized template class for satellite system information
  */
 template<SAT_SYS E, short int I>
-inline double freq ()
+inline constexpr double freq ()
 { return .0e0; }
 
 /** @brief Specialized template class for Gps satellite system,  band L1
  */
 template<>
-inline double freq<SAT_SYS::GPS, 1> ()
+inline constexpr double freq<SAT_SYS::GPS, 1> ()
 { return 1575.42e0; }
 
 /** @brief Specialized template class for Gps satellite system,  band L2
  */
 template<>
-inline double freq<SAT_SYS::GPS, 2> ()
+inline constexpr double freq<SAT_SYS::GPS, 2> ()
 { return 1227.60e0; }
 
 /** @brief Specialized template class for Gps satellite system,  band L5
  */
 template<>
-inline double freq<SAT_SYS::GPS, 5> ()
+inline constexpr double freq<SAT_SYS::GPS, 5> ()
 { return 1176.45e0; }
 
 } // end namespace ngpt
--- NEW/src/test.cpp
+++ NEW/src/test.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
 template<char C>
-class Attribute {};
+class Attribute {
+public:
+    constexpr Attribute(){};
+    constexpr char attribute () {return C;}
+};
 
 enum class ENUMR : unsigned char {
     SYSTEM1,
@@ -20,19 +24,36 @@ private:
     static constexpr char identifier { 'G' };
 };
 
-template<ENUMR Sys,short int Freq,
-    template <typename> class Attribute, typename C>
+template<ENUMR Sys,short int Freq,char C>
 class Obs {
 public:
     Obs():
         m_system{},
-        m_frequency{ Freq }//,
-        //m_a {a}
+        m_frequency{ Freq },
+        m_attribute{}
     {};
 private:
-    System<Sys> m_system;
-    short int   m_frequency;
-    //A           m_a;
+    System<Sys>  m_system;
+    short int    m_frequency;
+    Attribute<C> m_attribute;
+};
+
+template<ENUMR Sys,short int Freq,class Attribute>
+class Obs3 {
+public:
+    Obs3():
+        m_system{},
+        m_frequency{ Freq },
+        m_attribute{Attribute{}}
+    {};
+    constexpr char attribute () const 
+    {
+        return m_attribute.attribute();
+    }
+private:
+    System<Sys>  m_system;
+    short int    m_frequency;
+    Attribute    m_attribute;
 };
 
 int main ()
@@ -44,9 +65,15 @@ int main ()
 
     System<ENUMR::SYSTEM1> s3 (System<ENUMR::SYSTEM1>);
 
-    // Attribute<'M'> atr;
+    Attribute<'M'> atr;
 
-    Obs<ENUMR::SYSTEM1, 1, Attribute,char> obs;
+    Obs<ENUMR::SYSTEM1, 1, 'c'> obs;
+
+    Obs3<ENUMR::SYSTEM1, 1, Attribute<'X'> > obs3;
+    std::cout << "\nAttribute -> " << obs3.attribute ();
+
+    const char c { 'L' }; // removing the const, produces an error
+    Obs3<ENUMR::SYSTEM1, 1, Attribute<c> > obs4;
 
     std::cout <<"\n";
     return 0;
