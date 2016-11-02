0e83e3047eb03940d63dbe66b069defc2b01274e
xanthos <xanthos@mail.ntua.gr>
2016-11-02 13:06:55 +0200

    playing with qr

--- cpp/qr.cc
+++ cpp/qr.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 
 /// WARNING
 /// ---------------------------------------------
@@ -10,12 +11,85 @@
 /// * operator new should be in a try/catch block
 ///
 
+struct mat
+{
+    double* _dptr;
+    std::size_t _rows, _cols;
+
+    explicit mat(double* p = nullptr, std::size_t r = 0, std::size_t c = 0)
+    : _dptr(p), _rows(r), _cols(c)
+    {}
+
+    double&
+    operator()(std::size_t r, std::size_t c)
+    {
+        return _dptr[c*_rows+r];
+    }
+
+    double
+    operator()(std::size_t r, std::size_t c) const
+    {
+        return _dptr[c*_rows+r];
+    }
+
+    mat
+    submatrix(std::size_t row1, std::size_t row2, std::size_t col1, std::size_t col2)
+    {
+        assert( (row2 > row1) && (row2 <= this->_rows) );
+        assert( (col2 > col1) && (col2 <= this->_cols) );
+
+        std::size_t nr {this->_rows-(row2-row1)};
+        std::size_t nc {this->_cols-(col2-col1)};
+        
+        std::cout<<"\n\tNumber of rows for submatrix = "<<nr;
+        std::cout<<"\n\tNumber of cols for submatrix = "<<nc;
+        std::cout<<"\n\tStarting point is            = "<<this->operator()(row1, col1);
+
+        return mat{&(this->operator()(row1, col1)), nr, nc};
+    }
+
+    void
+    dump() const
+    {
+        for (std::size_t i = 0; i < this->_rows; ++i)
+        {
+            for (std::size_t j = 0; j < this->_cols; ++j)
+            {
+                std::cout<< " " << this->operator()(i, j);
+            }
+            std::cout<<"\n";
+        }
+    }
+};
+
+int main()
+{
+    std::size_t ROWS{5}, COLS{4};
+    double* nums = new double[ROWS*COLS];
+    double num{0};
+
+    for (std::size_t r=0;r<ROWS;++r)
+        for (std::size_t c=0;c<COLS;++c)
+            nums[r*COLS+c] = num++;
+
+    mat m{nums, ROWS, COLS};
+    std::cout<<"\nOriginal matrix size: "<<m._rows<<"x"<<m._cols<<"\n";
+    m.dump();
+
+    std::cout<<"Submatrix A(3:, 2:) is:\n";
+    auto m1 = m.submatrix(3, ROWS, 2, COLS);
+    m1.dump();
+
+    return 0;
+}
+
 /// \brief Resolve sub-matrix
 ///
 /// Given a matrix A with dimensions mxn, if we are interested in the sub-matrix
 /// A(j:m, i:n), then this function will provide a pointer to the first element
 /// of this (sub-)matrix and set its rows and columns.
 ///
+/*
 double*
 submatrix(const double* A, int m, int n, int j, int i, int& mm, int& nn)
 {
@@ -23,6 +97,7 @@ submatrix(const double* A, int m, int n, int j, int i, int& mm, int& nn)
     nn = n - i;
     return &A[m*i]+j;
 }
+*/
 
 ///
 /// \brief Householder Vector
@@ -41,6 +116,7 @@ submatrix(const double* A, int m, int n, int j, int i, int& mm, int& nn)
 ///                  >= n (the function will overwrite u[0:n)).
 /// \return          The scalar b (see description).
 ///
+/*
 double
 householder_vector(const double* x, int n, double* u)
 {
@@ -70,7 +146,7 @@ householder_vector(const double* x, int n, double* u)
     for (int i = 1; i < n; ++i) u[i] /= tmp;
 
     return beta;
-}
+}*/
 
 ///
 /// \brief QR decomposition via Householder.
@@ -93,6 +169,7 @@ householder_vector(const double* x, int n, double* u)
 /// \param[out] beta The scalar b (see description).
 /// \return
 ///
+/*
 void
 householder_qr(double* A, int n, int m)
 {
@@ -105,4 +182,4 @@ householder_qr(double* A, int n, int m)
         b   = householder_vector(Ajj, m-j);
         // A(j:m, j:n) = (I_(m-j+1)-buu^T)A(j:m,j:n)
     }
-}
+}*/
